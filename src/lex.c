aacea84fb8ad9b76f8f1d34b0550cf31f13787a9
Kyle Kloberdanz <kyle.g.kloberdanz@gmail.com>
2020-12-13 16:25:10 -0600

    need to fix token pushing logic

--- src/lex.c
+++ src/lex.c
@@ -7,25 +7,6 @@
 
 #define TOK_SIZE 1024
 
-static void push_token(
-    TokList **head,
-    TokList **curr,
-    const char *current_tok,
-    size_t lineno,
-    size_t col
-) {
-    printf("tok: %s\n", current_tok);
-    Token tok;
-    tok_init(&tok, lineno, col, current_tok, TOK_ID);
-    if (*curr) {
-        TokList *next = toklist_new(tok);
-        *curr = toklist_insert(*curr, next);
-    } else {
-        *curr = toklist_new(tok);
-        *head = *curr;
-    }
-}
-
 static char is_token(const char *tok) {
     UNUSED(tok);
     return 1;
@@ -46,13 +27,18 @@ static TokList *parse_line(const char *line, const size_t lineno) {
         /* try token with lookahead */
         if (is_token(current_tok)) {
             /* push token */
-            push_token(
-                &head,
-                &curr,
-                current_tok,
-                lineno,
-                col
-            );
+            Token tok;
+            printf("pushing: %s\n", current_tok);
+            tok_init(&tok, lineno, col, current_tok, TOK_ID);
+            if (curr) {
+                TokList *next = toklist_new(tok);
+                curr = toklist_insert(curr, next);
+            } else {
+                curr = toklist_new(tok);
+                head = curr;
+            }
+            toklist_print(head);
+
             idx = 0;
             col++;
             memset(current_tok, 0, TOK_SIZE);
@@ -60,13 +46,17 @@ static TokList *parse_line(const char *line, const size_t lineno) {
             /* unget char in token */
             current_tok[idx--] = '\0';
             if (is_token(current_tok)) {
-                push_token(
-                    &head,
-                    &curr,
-                    current_tok,
-                    lineno,
-                    col
-                );
+                Token tok;
+                printf("pushing: %s\n", current_tok);
+                tok_init(&tok, lineno, col, current_tok, TOK_ID);
+                if (curr) {
+                    TokList *next = toklist_new(tok);
+                    curr = toklist_insert(curr, next);
+                } else {
+                    curr = toklist_new(tok);
+                    head = curr;
+                }
+                toklist_print(head);
             }
             idx = 0;
             memset(current_tok, 0, TOK_SIZE);
@@ -93,8 +83,6 @@ TokList *lex(
     while ((getline(&line, &n, fp)) > 0) {
         TokList *tokens = parse_line(line, lineno);
         lineno++;
-
-        toklist_print(tokens);
 
         UNUSED(tokens);
     }
